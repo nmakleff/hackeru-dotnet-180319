622079eb95565024f0681c5b2baa96eb113eb3cd
nmakleff <nir@gurico.co.il>
2019-07-02 12:03:47 +0300

    advanced c# - lesson 2

---
+++ C# Advanced - Lesson 2 - Classes Cont/ClassesExample/ClassesEXample/Page.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEXample
+{
+    public class Page
+    {
+        private int _pageNumber;
+        private string _pageContent;
+        private bool _isChapterTitle;
+        private string _chapterTitle;
+        private int _chapterNumber;
+
+        public Page(int pageNumber, string pageContent, bool isChapterTitle, string chapterTitle, int chapterNumber)
+        {
+            _pageNumber = pageNumber;
+            _pageContent = pageContent;
+            _isChapterTitle = isChapterTitle;
+            _chapterTitle = chapterTitle;
+            _chapterNumber = chapterNumber;
+        }
+
+        public int GetPageNumber()
+        {
+            return _pageNumber;
+        }
+
+        public string GetContent()
+        {
+            return _pageContent;
+        }
+
+        public bool IsChapterTitle()
+        {
+            return _isChapterTitle;
+        }
+
+        public string GetChapterTitle()
+        {
+            return _chapterTitle;
+        }
+
+        public int GetChapterNumber()
+        {
+            return _chapterNumber;
+        }
+    }
+}
---
+++ C# Advanced - Lesson 2 - Classes Cont/ClassesExample/ClassesEXample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEXample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Nir", "My new book", "HackerU publishing", 10, 2019);
+
+            book.SetPage(new Page(1, "", true, "A new beginning", 1));
+            for (int i = 2; i <= 10; i++)
+            {
+                book.SetPage(new Page(i, "This is the content of page number " + i, false, "", 1));
+            }
+
+            BookViewer bookViewer = new BookViewer(book);
+            bookViewer.ViewBook();
+        }
+    }
+}
