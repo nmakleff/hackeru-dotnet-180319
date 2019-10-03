1e5f01947eb37cb1bdbcb557549530a39eb17c2f
Elad Lavi <eladlavi@users.noreply.github.com>
2019-10-03 19:36:20 +0300

    שיעור השלמה

---
+++ שיעור השלמה/Delegates-Sample/Delegates-Sample/CoffeeMaker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Sample
+{
+    class CoffeeMaker
+    {
+        public int Milk { get; set; }
+        public int Sugar { get; set; }
+        public int Coffee { get; set; }
+
+        public delegate void OutOfMilk(int Milk);
+        public delegate void OutOfSugar(int Sugar);
+        public delegate void OutOfCoffee(int Coffee);
+
+        public event OutOfMilk OnOutOfMilk;
+        public event OutOfSugar OnOutOfSugar;
+        public event OutOfCoffee OnOutOfCoffee;
+
+        public CoffeeMaker()
+        {
+            Milk = 1000;
+            Sugar = 10;
+            Coffee = 6;
+        }
+
+        public bool MakeCoffee(int numOfSugarSpoons, int numOfCoffeeSpoons)
+        {
+            if (Milk < 200 && OnOutOfMilk != null)
+                OnOutOfMilk(Milk);
+            if (Milk < 200)
+                return false;
+            Milk -= 200;
+
+            if (Sugar < numOfSugarSpoons && OnOutOfSugar != null)
+                OnOutOfSugar(Sugar);
+            if (Sugar < numOfSugarSpoons)
+                return false;
+            Sugar -= numOfSugarSpoons;
+
+            if (Coffee < numOfCoffeeSpoons && OnOutOfCoffee != null)
+                OnOutOfCoffee(Coffee);
+            if (Coffee < numOfCoffeeSpoons)
+                return false;
+            Coffee -= numOfCoffeeSpoons;
+
+            return true;
+        }
+
+    }
+}
---
+++ שיעור השלמה/Delegates-Sample/Delegates-Sample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Sample
+{
+    class Program
+    {
+        CoffeeMaker coffeeMaker;
+
+        public void FillUpMilk(int Milk)
+        {
+            Console.WriteLine("The coffee machine is out of milk. Adding 1 Liter");
+            coffeeMaker.Milk += 1000;
+            Console.WriteLine("{0} -> {1}", Milk, coffeeMaker.Milk);
+
+        }
+
+        public void FillUpCoffee(int Coffee)
+        {
+            Console.WriteLine("The coffee machine is out of coffee. Adding 10 spoons");
+            coffeeMaker.Coffee += 10;
+            Console.WriteLine("{0} -> {1}", Coffee, coffeeMaker.Coffee);
+        }
+
+        public void FillUpSugar(int Sugar)
+        {
+            Console.WriteLine("The coffee machine is out of sugar. Adding 10 spoons");
+            coffeeMaker.Sugar += 10;
+            Console.WriteLine("{0} -> {1}", Sugar, coffeeMaker.Sugar);
+        }
+
+        public void Run()
+        {
+            coffeeMaker = new CoffeeMaker();
+            coffeeMaker.OnOutOfMilk += FillUpMilk;
+            coffeeMaker.OnOutOfCoffee += FillUpCoffee;
+            coffeeMaker.OnOutOfSugar += FillUpSugar;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Create a cup of coffee");
+                coffeeMaker.MakeCoffee(2, 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.Run();
+        }
+    }
+}
