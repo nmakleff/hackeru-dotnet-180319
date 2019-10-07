0d9b1ceb081a2d5ce88fb12bb3a611b31a4c83dc
Elad Lavi <eladlavi@users.noreply.github.com>
2019-10-07 21:32:10 +0300

    Properties

---
+++ שיעור השלמה/Properties-Sample/Properties-Sample/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Sample
+{
+    class Human
+    {
+        private string _name;
+        private int _age;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value >= 0)
+                    if (value >= _age)
+                        if (value < 120)
+                            _age = value;
+            }
+        }
+
+        public Human(string name)
+        {
+            _name = name;
+            _age = 0;
+        }
+    }
+}
---
+++ שיעור השלמה/Properties-Sample/Properties-Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h = new Human("David");
+
+            h.Age = 20;
+            Console.WriteLine(h.Age);
+            h.Age = -1;
+            Console.WriteLine(h.Age);
+            h.Age = 19;
+            Console.WriteLine(h.Age);
+            h.Age = 21;
+            Console.WriteLine(h.Age);
+            h.Age = 200;
+            Console.WriteLine(h.Age);
+
+        }
+    }
+}
